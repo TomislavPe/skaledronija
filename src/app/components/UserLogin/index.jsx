190bd8c2512b886c9de58058af32cf52c80fbbb1
TomislavPe <130553449+TomislavPe@users.noreply.github.com>
2023-10-31 20:01:22 +0100

    prva verzija login komponente

---
+++ src/app/components/UserLogin/index.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+
+const UserLogin = ({ setUser }) => {
+	const [userName, setUserName] = useState("");
+	const [userColor, setUserColor] = useState("");
+
+	const handleUserNameChange = (e) => {
+		setUserName(e.target.value);
+	};
+
+	const handleUserColorChange = (e) => {
+		setUserColor(e.target.value);
+	};
+
+	const handleLogin = () => {
+		if (userName && userColor) {
+			setUser({ data: { name: userName, color: userColor } });
+		}
+	};
+
+	return (
+		<div>
+			<h2>User Login</h2>
+			<div>
+				<label htmlFor="userName">User Name:</label>
+				<input
+					type="text"
+					id="userName"
+					value={userName}
+					onChange={handleUserNameChange}
+				/>
+			</div>
+			<div>
+				<label htmlFor="userColor">User Color:</label>
+				<select
+					id="userColor"
+					value={userColor}
+					onChange={handleUserColorChange}
+				>
+					<option value="">Select a color</option>
+					<option value="red">Red</option>
+					<option value="blue">Blue</option>
+					<option value="green">Green</option>
+					<option value="yellow">Yellow</option>
+				</select>
+			</div>
+			<button onClick={handleLogin} disabled={!userName || !userColor}>
+				Login
+			</button>
+		</div>
+	);
+};
+
+export default UserLogin;
--- src/app/index.jsx
+++ src/app/index.jsx
@@ -2,13 +2,15 @@ import { useEffect, useState } from "react";
 import * as scaledrone from "./features/scaledroneApi";
 import NewMessage from "./components/NewMessage";
 import ChatMessages from "./components/ChatMessages";
+import UserLogin from "./components/UserLogin";
 import styles from "./app.module.css";
 
 function App() {
 	const [messages, setMessages] = useState([]);
-	const [user, setUser] = useState({
-		data: { name: "test korisnik", color: "green" },
-	});
+	// const [user, setUser] = useState({
+	// 	data: { name: "test korisnik", color: "green" },
+	// });
+	const [user, setUser] = useState(null);
 
 	const addMessage = (message) => {
 		setMessages((prevMessages) => {
@@ -26,12 +28,11 @@ function App() {
 
 	const onConnect = () => {
 		scaledrone.subscribeToMessages(handleReceivedMessage);
-		const userD = scaledrone.getUserId();
 		setUser({
 			...user,
 			data: {
 				...user.data,
-				id: userD,
+				id: scaledrone.getUserId(),
 			},
 		});
 	};
@@ -43,16 +44,18 @@ function App() {
 		return () => {
 			scaledrone.disconnect();
 		};
-	}, []);
+	}, [user]);
 
 	return (
 		<div className={styles.appContainer}>
-			{messages && user.data.id ? (
+			{messages && user ? (
 				<>
 					<ChatMessages messages={messages} user={user} />
 					<NewMessage sendMessageCallback={sendMessage} />
 				</>
-			) : null}
+			) : (
+				<UserLogin setUser={setUser}/>
+			)}
 		</div>
 	);
 }
