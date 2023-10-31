fb2e6ca20128d2c64cd9f4501be50da151285a25
TomislavPe <130553449+TomislavPe@users.noreply.github.com>
2023-10-31 11:20:40 +0100

    dodane testMessages za testiranje

--- src/app/components/ChatMessages/index.jsx
+++ src/app/components/ChatMessages/index.jsx
@@ -1,11 +1,14 @@
 import styles from "./ChatMessages.module.css";
+import testMessages from "./testMessages";
 
 const ChatMessages = ({ messages }) => {
+
 	return (
 		<div className={styles.chatMessagesContainer}>
 			<ul>
 				{messages &&
-					messages.map((message) => (
+					// messages.map((message) => (
+					testMessages.map((message) => (
 						<li key={message.id}>
 							User:{message.member.clientData.name}, Message:{" "}
 							{message.data}
---
+++ src/app/components/ChatMessages/testMessages.js
@@ -0,0 +1,164 @@
+const testMessages = [
+	{
+		member: {
+			clientData: {
+				name: "User 1",
+			},
+		},
+		data: "Hello, how can I help you?",
+	},
+	{
+		member: {
+			clientData: {
+				name: "User 2",
+			},
+		},
+		data: "I have a question about your product.",
+	},
+	{
+		member: {
+			clientData: {
+				name: "User 1",
+			},
+		},
+		data: "Sure, what's your question?",
+	},
+	{
+		member: {
+			clientData: {
+				name: "User 2",
+			},
+		},
+		data: "How much does it cost?",
+	},
+	{
+		member: {
+			clientData: {
+				name: "User 1",
+			},
+		},
+		data: "The product costs $50.",
+	},
+	{
+		member: {
+			clientData: {
+				name: "User 2",
+			},
+		},
+		data: "Thank you!",
+	},
+	{
+		member: {
+			clientData: {
+				name: "User 1",
+			},
+		},
+		data: "You're welcome!",
+	},
+	{
+		member: {
+			clientData: {
+				name: "User 3",
+			},
+		},
+		data: "Hello, can I join the conversation?",
+	},
+	{
+		member: {
+			clientData: {
+				name: "User 1",
+			},
+		},
+		data: "Of course, welcome User 3!",
+	},
+	{
+		member: {
+			clientData: {
+				name: "User 3",
+			},
+		},
+		data: "Thank you for welcoming me!",
+	},
+    {
+		member: {
+			clientData: {
+				name: "User 1",
+			},
+		},
+		data: "Hello, how can I help you?",
+	},
+	{
+		member: {
+			clientData: {
+				name: "User 2",
+			},
+		},
+		data: "I have a question about your product.",
+	},
+	{
+		member: {
+			clientData: {
+				name: "User 1",
+			},
+		},
+		data: "Sure, what's your question?",
+	},
+	{
+		member: {
+			clientData: {
+				name: "User 2",
+			},
+		},
+		data: "How much does it cost?",
+	},
+	{
+		member: {
+			clientData: {
+				name: "User 1",
+			},
+		},
+		data: "The product costs $50.",
+	},
+	{
+		member: {
+			clientData: {
+				name: "User 2",
+			},
+		},
+		data: "Thank you!",
+	},
+	{
+		member: {
+			clientData: {
+				name: "User 1",
+			},
+		},
+		data: "You're welcome!",
+	},
+	{
+		member: {
+			clientData: {
+				name: "User 3",
+			},
+		},
+		data: "Hello, can I join the conversation?",
+	},
+	{
+		member: {
+			clientData: {
+				name: "User 1",
+			},
+		},
+		data: "Of course, welcome User 3!",
+	},
+	{
+		member: {
+			clientData: {
+				name: "User 3",
+			},
+		},
+		data: "Thank you for welcoming me!",
+	},
+];
+
+export default testMessages;
