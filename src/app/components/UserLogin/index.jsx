8afaeb903cc12c415a96e99d362d91deb9a0e4b3
TomislavPe <130553449+TomislavPe@users.noreply.github.com>
2023-11-01 10:40:29 +0100

    login forma uređena, css uglavnom

--- src/app/components/UserLogin/index.jsx
+++ src/app/components/UserLogin/index.jsx
@@ -1,15 +1,12 @@
-import React, { useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import {
-	setConnected,
-	setUserName,
-	setUserColor,
-	setUserId,
-} from "../../features/scaledroneSlice";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { setUserName, setUserColor } from "../../features/scaledroneSlice";
+import styles from "./UserLogin.module.css";
 
 const UserLogin = ({ login }) => {
 	const [userName, setUserNameLocal] = useState("");
 	const [userColor, setUserColorLocal] = useState("");
+	const [loading, setLoading] = useState(false);
 	const dispatch = useDispatch();
 
 	const handleUserNameChange = (e) => {
@@ -22,6 +19,7 @@ const UserLogin = ({ login }) => {
 
 	const handleLogin = () => {
 		if (userName && userColor) {
+			setLoading(true);
 			dispatch(setUserName(userName));
 			dispatch(setUserColor(userColor));
 			login(userName, userColor);
@@ -29,34 +27,54 @@ const UserLogin = ({ login }) => {
 	};
 
 	return (
-		<div>
-			<h2>User Login</h2>
-			<div>
-				<label htmlFor="userName">User Name:</label>
-				<input
-					type="text"
-					id="userName"
-					value={userName}
-					onChange={handleUserNameChange}
-				/>
-			</div>
-			<div>
-				<label htmlFor="userColor">User Color:</label>
-				<select
-					id="userColor"
-					value={userColor}
-					onChange={handleUserColorChange}
-				>
-					<option value="">Select a color</option>
-					<option value="red">Red</option>
-					<option value="blue">Blue</option>
-					<option value="green">Green</option>
-					<option value="yellow">Yellow</option>
-				</select>
-			</div>
-			<button onClick={handleLogin} disabled={!userName || !userColor}>
-				Login
-			</button>
+		<div className={styles.centerContainer}>
+			{loading ? (
+				<h1>Spajanje...</h1>
+			) : (
+				<div>
+					<h2>Skaledronija</h2>
+					<p>Odaberite ime i boju</p>
+					<br />
+					<div className={styles.row}>
+						<label htmlFor="userName">Korisničko Ime: </label>
+						<div className={styles.formControl}>
+							<input
+								type="text"
+								id="userName"
+								value={userName}
+								onChange={handleUserNameChange}
+							/>
+						</div>
+						<label htmlFor="userColor">Boja Korisnika: </label>
+						<div className={styles.formControl}>
+							<select
+								id="userColor"
+								value={userColor}
+								onChange={handleUserColorChange}
+							>
+								<option value="">Odaberite boju</option>
+								<option value="red">Crvena</option>
+								<option value="blue">Plava</option>
+								<option value="green">Zelena</option>
+								<option value="yellow">Žuta</option>
+								<option value="orange">Narančasta</option>
+								<option value="purple">Ljubičasta</option>
+								<option value="pink">Roza</option>
+								<option value="brown">Smeđa</option>
+								<option value="grey">Siva</option>
+								<option value="black">Crna</option>
+							</select>
+						</div>
+						<button
+							className={styles.button}
+							onClick={handleLogin}
+							disabled={!userName || !userColor}
+						>
+							Prijava
+						</button>
+					</div>
+				</div>
+			)}
 		</div>
 	);
 };
