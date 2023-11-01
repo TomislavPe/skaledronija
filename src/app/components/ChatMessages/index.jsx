f1cd547f24031b9ae1e6875ab0997760cbd53f01
TomislavPe <130553449+TomislavPe@users.noreply.github.com>
2023-11-01 12:58:21 +0100

    uređen css u chatmessages

--- src/app/components/ChatMessages/index.jsx
+++ src/app/components/ChatMessages/index.jsx
@@ -4,23 +4,28 @@ import { useSelector } from "react-redux";
 const ChatMessages = () => {
 	const messages = useSelector((state) => state.messages.messagesList);
 	const userId = useSelector((state) => state.scaledrone.userId);
+
+	const getMessageStyle = (message) => {
+		return message.member.id == userId
+			? styles.rightMessage
+			: styles.leftMessage;
+	};
+
 	return (
 		<div className={styles.chatMessagesContainer}>
 			<ul className={styles.messageList}>
 				{messages.map((message) => (
 					<li
 						key={message.id}
-						className={`${styles.message} ${
-							message.member.id == userId
-								? styles.rightMessage
-								: styles.leftMessage
-						}`}
+						className={`${styles.message} ${getMessageStyle(message)}`}
 					>
-						<div className={styles.userName}>
+						<div
+							className={`${styles.userName} ${getMessageStyle(message)}`}
+						>
 							{message.member.clientData.name}
 						</div>
 						<div
-							className={styles.messageContent}
+							className={`${styles.messageContent} ${getMessageStyle(message)}`}
 							style={{
 								backgroundColor:
 									message.member.clientData.color,
